38cfaf9feeb8d58365a3d1191863cc04bb19a488
Jasmin Virdi <jasmin28virdi@gmail.com>
2023-05-19 01:56:08 +0530

    fetched data from file

--- src/home.js
+++ src/home.js
@@ -4,52 +4,52 @@ function Home() {
   return (
     <>
       <div className="header">
-        <h1>My Website</h1>
+        <h1 id="headerTitle">My Website</h1>
         <p>Resize the browser window to see the effect.</p>
       </div>
       <div className="topnav">
-        <a href="#">Link</a>
-        <a href="#">Link</a>
-        <a href="#">Link</a>
-        <a href="#" style={{ float: "right" }}>Link</a>
+        <a id="navbar-option-1" href="#">Link</a>
+        <a id="navbar-option-2" href="#">Link</a>
+        <a id="navbar-option-3" href="#">Link</a>
+        <a id="navbar-option-4" href="#" style={{ float: "right" }}>Link</a>
       </div>
       <div className="row">
         <div className="leftcolumn">
-          <div className="card">
-            <h2>TITLE HEADING</h2>
-            <h5>Title description, Dec 7, 2017</h5>
+          <div id="card1" className="card">
+            <h2 id="card1-h2">TITLE HEADING</h2>
+            <h5 id="card1-h5">Title description, Dec 7, 2017</h5>
             <div className="fakeimg" style={{ height: "200px" }}>Image</div>
-            <p>Some text..</p>
-            <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+            <p id="card1-p1">Some text..</p>
+            <p id="card1-p2">Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
           </div>
           <div className="card">
             <h2>TITLE HEADING</h2>
             <h5>Title description, Sep 2, 2017</h5>
             <div className="fakeimg" style={{ height: "200px" }}>Image</div>
-            <p>Some text..</p>
-            <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+            <p id="p1">Some text..</p>
+            <p id="p2">Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
           </div>
         </div>
         <div className="rightcolumn">
-          <div className="card">
+          <div id="card3" className="card">
             <h2>About Me</h2>
             <div className="fakeimg" style={{ height: "100px" }}>Image</div>
-            <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+            <p id="p1"></p>
           </div>
-          <div className="card">
-            <h3>Popular Post</h3>
-            <div className="fakeimg"><p>Image</p></div>
-            <div className="fakeimg"><p>Image</p></div>
-            <div className="fakeimg"><p>Image</p></div>
+          <div id="card4" className="card">
+            <h3 id="card4-h3">Popular Post</h3>
+            <div id="card4-img1" className="fakeimg"><p>Image</p></div>
+            <div id="card4-img2" className="fakeimg"><p>Image</p></div>
+            <div id="card4-img3" className="fakeimg"><p>Image</p></div>
           </div>
-          <div className="card">
-            <h3>Follow Me</h3>
-            <p>Some text..</p>
+          <div id="card5" className="card">
+            <h3 id="card5-h3">Follow Me</h3>
+            <p id="card5-p">Some text..</p>
           </div>
         </div>
       </div>
 
-      <div class="footer">
+      <div id="footer-h2" class="footer">
         <h2>Footer</h2>
       </div>
 
